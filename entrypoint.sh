99d3e3fe090d342dd381db0cdb7b79d0f12781c2
Athreya Patel <athreyapatel@gmail.com>
2020-10-11 11:46:27 +0000

    Timestamp

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,15 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "{'timestamp':'$D','severity':'ERROR','message':'An error is usually an exception that has been caught and not handled'}"
+      "1") echo '{"severity":"ERROR","level":50,"timestamp":"2020-09-30T20:19:06.210Z","pid":17,"hostname":"123","name":"xyz","message":"RequestId: 9fcf7a84-73a7-4bcd-b022-c63afaee8d3c, route: /api/v1/transfer/?after=2020-09-30T04:14:08.290779Z, method: GET, appBuildEnv: dev, appVersion :1.7.02 An error occurred while fetching logs error:"}'
       ;;
-      "2") echo "{'timestamp':'$D','severity':'INFO','message':'This is less important than debug log and is often used to provide context in the current task.'}"
+      "2") echo '{"severity":"CRITICAL","level":50,"timestamp":"2020-09-30T20:19:06.437Z","pid":17,"hostname":"india","name":"gg","message":"RequestId: 9fcf7a84-73a7-4bcd-b022-c63afaee8d3c, route: /api/v1/transfer/?after=2020-09-30T04:14:08.290779Z, method: GET, appBuildEnv: dev, appVersion :1.7.02 An error occurred while fetching logs error:"}'
       ;;
-      "3") echo "{'timestamp':'$D','severity':'WARN','message':'A warning that should be ignored is usually at this level and should be actionable.'}"
+      "3") echo '{"severity":"INFO","level":50,"timestamp":"2020-09-30T20:19:06.716Z","pid":17,"hostname":"mandya","name":"gg2","message":"RequestId: 9fcf7a84-73a7-4bcd-b022-c63afaee8d3c, route: /api/v1/transfer/?after=2020-09-30T04:14:08.290779Z, method: GET, appBuildEnv: dev, appVersion :1.7.02 An error occurred while fetching logs error:"}'
       ;;
-      "4") echo "{'timestamp':'$D','severity':'DEBUG','message':'This is a debug log that shows a log that can be ignored.'}"
+      "4") echo '{"severity":"DEBUG","level":50,"timestamp":"2020-09-30T20:19:06.786Z","pid":17,"hostname":"gg","name":"gg3","message":"RequestId: 9fcf7a84-73a7-4bcd-b022-c63afaee8d3c, route: /api/v1/transfer/?after=2020-09-30T04:14:08.290779Z, method: GET, appBuildEnv: dev, appVersion :1.7.02 An error occurred while fetching logs error:"}'
       ;;
    esac
    c=$(( c+1 ))
 done
+2020-09-30T10:07:18+00:00
