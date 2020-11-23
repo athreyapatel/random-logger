c3c85c784a21c1f1857cef498bd1bea4d240f070
Athreya Patel <athreyapatel@gmail.com>
2020-11-23 07:11:24 +0000

    Added err reporting api

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo '{"severity":"ERROR","level":50,"timestamp":"2020-09-30T20:19:06.210Z","pid":17,"hostname":"123","name":"xyz","message":"RequestId: 9fcf7a84-73a7-4bcd-b022-c63afaee8d3c, route: /api/v1/transfer/?after=2020-09-30T04:14:08.290779Z, method: GET, appBuildEnv: dev, appVersion :1.7.02 An error occurred while fetching logs error:"}'
+      "1") echo '{"severity":"ERROR","@type":"type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent","level":50,"timestamp":"2020-09-30T20:19:06.210Z","pid":17,"hostname":"123","name":"xyz","message":"RequestId: 9fcf7a84-73a7-4bcd-b022-c63afaee8d3c, route: /api/v1/transfer/?after=2020-09-30T04:14:08.290779Z, method: GET, appBuildEnv: dev, appVersion :1.7.02 An error occurred while fetching logs error:"}'
       ;;
       "2") echo '{"severity":"CRITICAL","level":50,"timestamp":"2020-09-30T20:19:06.437Z","pid":17,"hostname":"india","name":"gg","message":"RequestId: 9fcf7a84-73a7-4bcd-b022-c63afaee8d3c, route: /api/v1/transfer/?after=2020-09-30T04:14:08.290779Z, method: GET, appBuildEnv: dev, appVersion :1.7.02 An error occurred while fetching logs error:"}'
       ;;
@@ -24,4 +24,3 @@ do
    esac
    c=$(( c+1 ))
 done
-2020-09-30T10:07:18+00:00
